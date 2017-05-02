369af9b024c211aeaad407e1ea25080fed7181bc
Connor Clark <cjamcl@gmail.com>
2017-05-02 14:33:05 -0700

    fix path issue with default toc template

--- bin/cli.js
+++ bin/cli.js
@@ -4,12 +4,13 @@ var fs = require('fs')
 var expand = require('glob-expand')
 var toccer = require('../')
 var utils = require('../lib/utils')
+var path = require('path')
 
 var args = utils.flatten(process.argv.slice(2).map(function (arg) {
   return expand(arg, '!node_modules/**/*.*')
 }))
 
-var defaultTocTemplate = fs.readFileSync('toc.mustache', 'utf8')
+var defaultTocTemplate = fs.readFileSync(path.join(__dirname, '..', 'toc.mustache'), 'utf8')
 
 args.forEach(function (fileName) {
   var input = fs.readFileSync(fileName, 'utf8')
